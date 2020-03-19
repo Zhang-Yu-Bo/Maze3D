b5d7053edccb672824d3e669b6ec30d496bb86ea
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-03-19 16:57:09 +0800

    [更新] 設定角色動畫 (尚未完成)

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [Header("最大速率")]
+    public float MAX_SPEED;
+    [Header("最小速率")]
+    public float MIN_SPEED;
+    [Header("現在速率")]
+    public float mSpeed;
+
+    private Animator mAnimator;
+
+    void Awake()
+    {
+        // initial value
+        this.mSpeed = 0.0f;
+        this.mAnimator = this.gameObject.GetComponent<Animator>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.mSpeed += Time.deltaTime;
+        }
+        else
+        {
+            this.mSpeed -= Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            this.mAnimator.SetBool("Crouch", true);
+        }
+        else
+        {
+            this.mAnimator.SetBool("Crouch", false);
+        }
+
+        this.mSpeed = Mathf.Clamp(this.mSpeed, this.MIN_SPEED, this.MAX_SPEED);
+        this.mAnimator.SetFloat("Speed", this.mSpeed);
+    }
+}
