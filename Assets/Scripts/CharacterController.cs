80147472d3343956fedb821ad96fed7dc95c5258
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-03-19 17:56:48 +0800

    [更新] 角色控制腳本(尚未完成)

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -10,13 +10,18 @@ public class CharacterController : MonoBehaviour
     public float MIN_SPEED;
     [Header("現在速率")]
     public float mSpeed;
+    [Header("跳躍力")]
+    public float jumpPower;
+
 
     private Animator mAnimator;
+    public bool onAir;
 
     void Awake()
     {
         // initial value
         this.mSpeed = 0.0f;
+        this.onAir = true;
         this.mAnimator = this.gameObject.GetComponent<Animator>();
     }
 
@@ -29,6 +34,13 @@ public class CharacterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        this.moveControll();
+        this.animationControll();
+    }
+
+    private void moveControll()
+    {
+        // 
         if (Input.GetKey(KeyCode.W))
         {
             this.mSpeed += Time.deltaTime;
@@ -37,16 +49,38 @@ public class CharacterController : MonoBehaviour
         {
             this.mSpeed -= Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            this.mAnimator.SetBool("Crouch", true);
-        }
-        else
-        {
-            this.mAnimator.SetBool("Crouch", false);
-        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && !this.onAir)
+        {
+            this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * this.jumpPower);
+            this.mAnimator.SetTrigger("Jump");
+        }
         this.mSpeed = Mathf.Clamp(this.mSpeed, this.MIN_SPEED, this.MAX_SPEED);
+    }
+
+    private void animationControll()
+    {
         this.mAnimator.SetFloat("Speed", this.mSpeed);
+        if (Input.GetKey(KeyCode.LeftControl))
+            this.mAnimator.SetBool("Crouch", true);
+        else
+            this.mAnimator.SetBool("Crouch", false);
+        if (Input.GetKey(KeyCode.LeftShift))
+            this.mAnimator.SetBool("Run", true);
+        else
+            this.mAnimator.SetBool("Run", false);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            this.onAir = false;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Floor")
+            this.onAir = true;
     }
 }
