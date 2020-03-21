e6ec7e1d3cf1bb32fb7b33a43519567fbc9a4a85
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-03-21 18:22:58 +0800

    [更新] 倒數計時功能

---
+++ Assets/Scripts/MazeUIHandler.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MazeUIHandler : MonoBehaviour
+{
+    public Text timeText;
+
+    private float Mins;
+    private float Secs;
+    private float mSecs;
+    private bool isEnd;
+
+    private void Awake()
+    {
+        this.isEnd = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.Mins = 5;
+        this.Secs = 0;
+        this.mSecs = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.updateTime();
+    }
+
+    private void updateTime()
+    {
+        float deltaTime = Mathf.Floor(Time.deltaTime * 100);
+        this.mSecs -= deltaTime;
+        if (this.mSecs < 0)
+        {
+            this.Secs--;
+            this.mSecs += 100;
+        }
+        if (this.Secs < 0)
+        {
+            this.Mins--;
+            this.Secs += 60;
+        }
+        if (this.Mins >= 0)
+            timeText.text = "Time: " + this.Mins.ToString("00") + ":" + this.Secs.ToString("00") + ":" + this.mSecs.ToString("00");
+        else if (!this.isEnd)
+        {
+            Debug.Log("End");
+            this.isEnd = true;
+        }
+    }
+}
