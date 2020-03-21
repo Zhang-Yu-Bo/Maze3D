a23b3c1c79c12f09c55302a02e81f48195004370
Zhang-Yu-Bo <46323902+Zhang-Yu-Bo@users.noreply.github.com>
2020-03-21 23:49:28 +0800

    [更新] FinalScene、傳送

---
+++ Assets/Scripts/ExitScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitScript : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+        SceneManager.LoadScene("FinalScene");
+    }
+}
