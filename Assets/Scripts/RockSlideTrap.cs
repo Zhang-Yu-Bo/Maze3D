4e7e599d40a65e505a344983ba2b6b1ef628907a
mosquitoYee <kenwen1999@gmail.com>
2020-03-22 21:42:14 +0800

    [Update]新增迷宮機關&說明文件更新

---
+++ Assets/Scripts/RockSlideTrap.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockSlideTrap : MonoBehaviour
+{
+	public Rigidbody[] rock;
+
+	void Start()
+	{
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("HI");
+		for(int i=0;i<18;i++)
+		{
+			rock[i].GetComponent<MeshRenderer>().enabled = true;
+			rock[i].GetComponent<Rigidbody>().useGravity = true;
+		}
+	}
+}
---
+++ Assets/Scripts/WallsRiseTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallsRiseTrigger : MonoBehaviour
+{
+
+	public Animator wallsRiseAni;
+    // Start is called before the first frame update
+    void Start()
+    {
+		wallsRiseAni.enabled=false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	void OnTriggerEnter(Collider other)
+	{
+		wallsRiseAni.enabled=true;
+	}
+}
